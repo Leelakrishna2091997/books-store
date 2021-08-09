49125ef17879492eed3a558a570506701df6a93d
Leela Krishna <u723102>
2021-08-09 21:22:10 +0530

    forgot, verify added

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { config } from 'src/config';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,12 +11,14 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
   newPassword = new FormControl('', Validators.required);
   confirmPassword = new FormControl('', Validators.required);
   answer = new FormControl('', Validators.required);
   hintQuestion = new FormControl('');
   userName = new FormControl('', Validators.required);
+  public hintsArray: any;
+  public isVerified = false;
   //   {
   //     "username": "spandan",
   //     "new_password": "passwor",
@@ -21,7 +26,35 @@ export class ForgotPasswordComponent implements OnInit {
   //     "hint_answer": "blue"
   // }
   ngOnInit(): void {
+    this.getHints();
   }
-  submit() { }
-  submitQuestion() { }
+  getHints() {
+    this.http.get(config.serverName + ':' + config.port + '/hints').subscribe(res => {
+      this.hintsArray = res;
+    })
+  }
+
+  submit() {
+    this.http.post(config.serverName + ":" + config.port + '/reset-password',
+      { "username": this.userName.value, "new_password": this.confirmPassword.value }, { observe: 'response' }).subscribe(res => {
+        console.log(res);
+        if (res && res.status == 204) {
+          this.router.navigateByUrl('login')
+        }
+
+      });
+
+  }
+  submitQuestion() {
+    // {"username": "leela","hq_id": "2", "hint_answer": "Bye"}
+    this.http.post(config.serverName + ":" + config.port + '/verify',
+      { "username": this.userName.value, "hq_id": this.hintQuestion.value, "hint_answer": this.answer.value }, { observe: 'response' }).subscribe(res => {
+        console.log(res);
+        if (res && res.status == 200) {
+          this.isVerified = true;
+        }
+
+      });
+
+  }
 }
